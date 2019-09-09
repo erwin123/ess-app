76219b0dda60ba6302d2006a34a96144474cb980
erwin antonius <erwin.ant@gmail.com>
2019-09-09 17:32:27 +0700

    add maintenance location

--- src/app/main/admin/admin.module.ts
+++ src/app/main/admin/admin.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { SetLocationComponent } from './set-location/set-location.component';
 import { SharingModuleModule } from 'src/app/sharing-module/sharing-module.module';
+import { MaintainLocationComponent } from './maintain-location/maintain-location.component';
 
 @NgModule({
-  declarations: [SetLocationComponent],
+  declarations: [SetLocationComponent, MaintainLocationComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
---
+++ src/app/main/admin/maintain-location/maintain-location.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maintain-location',
+  templateUrl: './maintain-location.component.html',
+  styleUrls: ['./maintain-location.component.scss']
+})
+export class MaintainLocationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
